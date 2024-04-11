6995a4635f6120ce71c7d24976bbb07581e55254
Viraj Shelke <virajshelkepro@gmail.com>
2024-04-11 17:07:47 +0530

    understanding packages better

---
+++ 4.packages/main.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+state.go file contains the 'printState' function. That file also belongs to same package main.
+The functions from the smae packages need not be imported.
+
+To build this code we need to write 'go build .\4.packages\main.go .\4.packages\state.go'
+*/
+
+func main() {
+	printState()
+}
---
+++ 4.packages/state.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+/*
+To build this code we need to write 'go build .\4.packages\main.go .\4.packages\state.go'
+
+'printState' is used in main.go
+*/
+
+func printState() {
+	fmt.Println("California")
+}
